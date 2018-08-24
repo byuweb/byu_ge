fdffdc83b45ca39d5c0ac210fe5afe5e69c90728
William Wallace <willtwall@googlemail.com>
2018-08-24 08:31:22 -0600

    Added templates for faculty view.

--- templates/node--view--faculty.tpl.php
+++
@@ -1,4 +0,0 @@
-<a href="<?php print $href ?>" class="circle-card">
-  <div class="circle-image"><img src="<?php print $image ?>"></div>
-  <div class="circle-button"><?php print $title ?></div>
-</a>
