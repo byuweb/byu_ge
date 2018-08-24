cf201c4fa166a26d0aca3541a99e017ccab0ef63
William Wallace <willtwall@googlemail.com>
2018-08-24 14:12:37 -0600

    Finished templates for contact us page.

--- js/content.js
+++ js/content.js
@@ -1,5 +1,18 @@
 (function($) {
   $(document).ready(function() {
     $('.view-foundation-documents').before("<div class='foundation-document-image'></div>");
+
+    $('.contact-card .views-field-body').after('<div class="contact"></div>');
+    let contacts = $('.contact-card .contact');
+    var i;
+
+    for (i = 1; i <= contacts.length; i++) {
+      let contact = $('.views-row-' + i + ' .contact-card .contact');
+      let phone = $('.views-row-' + i + ' .contact-card .views-field-field-phone');
+      let email = $('.views-row-' + i + ' .contact-card .views-field-field-e-mail');
+      let location = $('.views-row-' + i + ' .contact-card .views-field-field-room-number');
+
+      contact.append(phone).append(location).append(email);
+    }
   });
 })(jQuery);
--- template.php
+++ template.php
@@ -1,51 +1,4 @@
 <?php
-function byu_ge_preprocess_node(&$variables) {
-  $filepath = variable_get('file_public_path', conf_path() . '/files/');
-  if ($variables['type'] == 'student_page' || $variables['type'] == 'faculty_page') {
-    $variables['image'] = $filepath . $variables['field_image'][0]['filename'];
-    $variables['href'] = '?q=node/' . $variables['nid'];
-  }
-
-  if ($variables['type'] == 'contact') {
-    $variables['filepath'] = $filepath;
-    if (isset($variables['field_image'])) {
-      $variables['image'] = $filepath . $variables['field_image'][0]['filename'];
-    }
-    $variables['position'] = $variables['field_position'][0]['value'];
-    $variables['description'] = $variables['body'][0]['value'];
-    $variables['phone'] = $variables['field_phone'][0]['value'];
-    $variables['email'] = $variables['field_e_mail'][0]['value'];
-    $variables['location'] = $variables['field_room_number'][0]['value'];
-  }
-
-  if ($variables['type'] == 'office_contact') {
-    $variables['phone'] = $variables['field_phone'][0]['value'];
-    $variables['email'] = $variables['field_e_mail'][0]['value'];
-    $variables['location'] = $variables['field_room_number'][0]['value'];
-  }
-
-  if ($variables['type'] == 'social_media_links') {
-    $variables['facebook'] = !empty($variables['field_facebook']) ? $variables['field_facebook'][0]['url'] : '';
-    $variables['twitter'] = !empty($variables['field_twitter']) ? $variables['field_twitter'][0]['url'] : '';
-    $variables['instagram'] = !empty($variables['field_instagram']) ? $variables['field_instagram'][0]['url'] : '';
-    $variables['youtube'] = !empty($variables['field_youtube']) ? $variables['field_youtube'][0]['url'] : '';
-    $variables['pinterest'] = !empty($variables['field_pinterest']) ? $variables['field_pinterest'][0]['url'] : '';
-    $variables['googleplus'] = !empty($variables['field_google_plus']) ? $variables['field_google_plus'][0]['url'] : '';
-    $variables['linkedin'] = !empty($variables['field_linkedin']) ? $variables['field_linkedin'][0]['url'] : '';
-    $variables['rss'] = !empty($variables['field_rss']) ? $variables['field_rss'][0]['url'] : '';
-    $variables['snapchat'] = !empty($variables['field_snapchat']) ? $variables['field_snapchat'][0]['url'] : '';
-  }
-
-  if ($variables['type'] == 'foundation_document') {
-    $variables['href'] = $variables['field_doc_link'][0]['url'];
-    $variables['cardColor'] = $variables['field_color'][0]['value'];
-  }
-
-  if ($variables['type'] == 'front_page_link') {
-    $variables['href'] = '?q=' . $variables['field_page_link'][0]['value'];
-    $variables['image'] = $filepath . $variables['field_image'][0]['filename'];
-  }
-}
 
 function byu_ge_preprocess_views_view(&$variables) {
   $path = 'sites/all/themes/byu_ge/';
@@ -57,6 +10,7 @@ function byu_ge_preprocess_views_view(&$variables) {
       break;
     case 'contact_us':
       drupal_add_css($path . 'css/contact-card.css');
+      drupal_add_js($path . 'js/content.js');
       break;
     case 'core_curriculum':
     case 'course_outcomes':
@@ -72,4 +26,8 @@ function byu_ge_preprocess_views_view(&$variables) {
       drupal_add_css($path . 'css/office-contact.css');
       break;
   }
+}
+
+function byu_ge_preprocess_views_view_field(&$variables) {
+  $variables['filepath'] = variable_get('file_public_path', conf_path() . '/files');
 }
