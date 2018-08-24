e4067821d5a7d25a437b599e557cbcba532bc2a6
William Wallace <willtwall@googlemail.com>
2018-08-24 09:01:26 -0600

    Added templates for office contact view.

--- templates/node--office-contact.tpl.php
+++
@@ -1,5 +0,0 @@
-<div class="office-contact">
-  <div class="office-phone"><?php print $phone ?></div>
-  <div class="office-location"><?php print $location ?></div>
-  <div class="office-email"><?php print $email ?></div>
-</div>
--- templates/node--view--front-page-links.tpl.php
+++
@@ -1,6 +0,0 @@
-<a href="<?php print $href ?>">
-  <div class="title-area">
-    <div class="text-wrapper"><?php print $title ?></div>
-  </div>
-  <img src="<?php print $image ?>">
-</a>
